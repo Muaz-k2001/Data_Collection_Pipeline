ba073e75140a517b494c168f5df92985096c0312
Muaz Khan <muaz.k2001@gmail.com>
2022-07-06 16:25:00 +0100

    Adds get_property_links function for 1 page

--- scraper.py
+++ scraper.py
@@ -11,11 +11,13 @@ import time
 
 class Scraper:
        
-        
+    def __init__(self):
+        self.property_list = []
 
 
         
     def accept_cookies(self, driver):
+        time.sleep(3)
         try:
             WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id= "gdpr-consent-notice"]')))
             print('Found pop-up')
@@ -31,18 +33,37 @@ class Scraper:
 
 
     def search_ng8(self, driver):
-        search_bar = driver.find_element('xpath', '//*[@class="c-voGFy"]')
+        time.sleep(1)
+        search_bar = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@class="c-voGFy"]')))
         search_bar.click()
         search_bar.send_keys('NG8  Nottingham, Wollaton, Aspley')
         search_bar.send_keys(Keys.RETURN)
+        return driver
+
+
+    def get_property_links(self, driver):
+        time.sleep(3)
+        properties = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class= "css-1itfubx er561h10"]/div')))
+        property_list = []
+        for property in properties:
+            a_tag = property.find_element(By.TAG_NAME, 'a')
+            property_link = a_tag.get_attribute('href')
+            property_list.append(property_link)
+        print(len(property_list))
+
+    def get_property_info(self, driver):
+        pass
+    pass
 
 
 
 def start(driver, URL):
     driver.get(URL)
+    driver.maximize_window()
     auto = Scraper()
-    auto.accept_cookies(driver)
-    auto.search_ng8(driver)
+    driver = auto.accept_cookies(driver)
+    driver = auto.search_ng8(driver)
+    auto.get_property_links(driver)
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
