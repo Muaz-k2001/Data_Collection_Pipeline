89c81c2efb5a14175464faebf3e11124ac33ead6
Muaz Khan <muaz.k2001@gmail.com>
2022-07-06 13:47:50 +0100

    Adds function to search for ng8

--- scraper.py
+++ scraper.py
@@ -3,16 +3,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+import time
+
+# class Scraper:
 
 
-#class Scraper:
-
-
-def load_page() -> webdriver.Firefox:
-    driver = webdriver.Firefox()
+def load_page():
+    driver = webdriver.Chrome()
     URL = 'https://www.zoopla.co.uk/'
     driver.get(URL)
     accept_cookies(driver)
+    search_ng8(driver)
+
+
     
 def accept_cookies(driver):
     try:
@@ -22,7 +26,20 @@ def accept_cookies(driver):
         accept_cookies_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id= "save"]')))
         accept_cookies_button.click()
         print('Button Clicked')
+        return driver
     except TimeoutException:
         print('Button not found')
 
+
+
+
+def search_ng8(driver):
+    search_bar = driver.find_element('xpath', '//*[@class="c-voGFy"]')
+    search_bar.click()
+    search_bar.send_keys('NG8  Nottingham, Wollaton, Aspley')
+    search_bar.send_keys(Keys.RETURN)
+    print('hello')
+
+
 load_page()
+
