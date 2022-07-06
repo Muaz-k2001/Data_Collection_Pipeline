24e3ccb5a8b542de8c28af9715e1f80f022cb56a
Muaz Khan <muaz.k2001@gmail.com>
2022-07-06 14:01:35 +0100

    Puts created functions into scraper class

--- scraper.py
+++ scraper.py
@@ -6,40 +6,43 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 import time
 
-# class Scraper:
+driver = webdriver.Chrome()
+URL = 'https://www.zoopla.co.uk/'
+
+class Scraper:
+       
+        
 
 
-def load_page():
-    driver = webdriver.Chrome()
-    URL = 'https://www.zoopla.co.uk/'
+        
+    def accept_cookies(self, driver):
+        try:
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id= "gdpr-consent-notice"]')))
+            print('Found pop-up')
+            driver.switch_to.frame('gdpr-consent-notice')
+            accept_cookies_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id= "save"]')))
+            accept_cookies_button.click()
+            print('Button Clicked')
+            return driver
+        except TimeoutException:
+            print('Button not found')
+
+
+
+
+    def search_ng8(self, driver):
+        search_bar = driver.find_element('xpath', '//*[@class="c-voGFy"]')
+        search_bar.click()
+        search_bar.send_keys('NG8  Nottingham, Wollaton, Aspley')
+        search_bar.send_keys(Keys.RETURN)
+        print('hello')
+
+def start(driver, URL):
     driver.get(URL)
-    accept_cookies(driver)
-    search_ng8(driver)
+    r = Scraper()
+    r.accept_cookies(driver)
+    r.search_ng8(driver)
 
 
-    
-def accept_cookies(driver):
-    try:
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id= "gdpr-consent-notice"]')))
-        print('Found pop-up')
-        driver.switch_to.frame('gdpr-consent-notice')
-        accept_cookies_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id= "save"]')))
-        accept_cookies_button.click()
-        print('Button Clicked')
-        return driver
-    except TimeoutException:
-        print('Button not found')
-
-
-
-
-def search_ng8(driver):
-    search_bar = driver.find_element('xpath', '//*[@class="c-voGFy"]')
-    search_bar.click()
-    search_bar.send_keys('NG8  Nottingham, Wollaton, Aspley')
-    search_bar.send_keys(Keys.RETURN)
-    print('hello')
-
-
-load_page()
+start(driver, URL)
 
