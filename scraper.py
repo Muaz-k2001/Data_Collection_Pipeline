045b5ae2fba7f84d36e690964a6ab83bc131b2d8
Muaz Khan <muaz.k2001@gmail.com>
2022-07-07 14:46:52 +0100

    Adds function and loop to change to next page 4 times to get all property links

--- scraper.py
+++ scraper.py
@@ -1,4 +1,3 @@
-from enum import auto
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,16 +12,19 @@ class Scraper:
        
     def __init__(self):
         self.property_list = []
-
+        self.page_count = 0
+        self.big_list = []
 
         
+
+
     def accept_cookies(self, driver):
         time.sleep(3)
         try:
-            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id= "gdpr-consent-notice"]')))
+            WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id= "gdpr-consent-notice"]')))
             print('Found pop-up')
             driver.switch_to.frame('gdpr-consent-notice')
-            accept_cookies_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id= "save"]')))
+            accept_cookies_button = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id= "save"]')))
             accept_cookies_button.click()
             print('Button Clicked')
             return driver
@@ -34,21 +36,31 @@ class Scraper:
 
     def search_ng8(self, driver):
         time.sleep(1)
-        search_bar = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@class="c-voGFy"]')))
+        search_bar = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@class="c-voGFy"]')))
         search_bar.click()
         search_bar.send_keys('NG8  Nottingham, Wollaton, Aspley')
         search_bar.send_keys(Keys.RETURN)
         return driver
 
 
+
     def get_property_links(self, driver):
-        time.sleep(3)
-        properties = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class= "css-1itfubx er561h10"]/div')))
+        try:
+            close_button = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '//*[@class= "css-e4jnh6-CancelButton e13xjwxo6"]')))
+            close_button.click()
+        except:
+            pass
+        properties = WebDriverWait(driver, 100).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@class= "css-1itfubx ez2h2380"]/div')))
+        self.property_list.clear()
         for property in properties:
             a_tag = property.find_element(By.TAG_NAME, 'a')
             property_link = a_tag.get_attribute('href')
             self.property_list.append(property_link)
-        print(len(self.property_list))
+        print('got links')
+        return self.property_list
+
+
+
 
     def get_property_info(self, driver):
         pass
@@ -56,15 +68,32 @@ class Scraper:
 
 
 
-def start(driver, URL):
-    driver.get(URL)
-    driver.maximize_window()
-    auto = Scraper()
-    driver = auto.accept_cookies(driver)
-    driver = auto.search_ng8(driver)
-    auto.get_property_links(driver)
+
+    def change_page(self, driver):
+        next_page = driver.find_element(By.XPATH, '//*[@class= "css-qhg1xn-PaginationItemPreviousAndNext-PaginationItemNext eaoxhri2"]')
+        next_page.click()
+
+
+
+
+    def start(self, driver, URL):
+        page_counter = 0
+        driver.get(URL)
+        # driver.maximize_window()
+        self.accept_cookies(driver)
+        self.search_ng8(driver)
+        while page_counter < 4:
+            page_counter += 1
+            self.get_property_links(driver)
+            self.big_list.extend(self.property_list)
+            self.change_page(driver)
+        if page_counter == 4:
+            self.get_property_links(driver)
+            self.big_list.extend(self.property_list)
+        print(len(self.big_list))
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
     URL = 'https://www.zoopla.co.uk/'
-    start(driver, URL)
+    p = Scraper()
+    p.start(driver, URL)
