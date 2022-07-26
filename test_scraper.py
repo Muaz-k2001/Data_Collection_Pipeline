5b09d5b12ba61e2fc62696ec18a6cb7494fd7c02
Muaz Khan <muaz.k2001@gmail.com>
2022-07-26 14:53:45 +0100

    Adds test functions for data file contents and downloaded images

--- test_scraper.py
+++ test_scraper.py
@@ -2,17 +2,11 @@ from cmath import exp
 from scraper import Scraper
 from selenium import webdriver
 import unittest
-import uuid
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
 import time
 import os
-import shutil
-import urllib.request
-import tempfile
+import json
 
 
 class ScraperTestCase(unittest.TestCase):
@@ -24,74 +18,52 @@ class ScraperTestCase(unittest.TestCase):
         self.scraper.accept_cookies()
         self.scraper.search_ng8()
         time.sleep(2)
+        property_list = self.scraper.get_property_links()
+        self.scraper.big_list.extend(property_list)
 
 
-    # def test_accept_cookies(self):
-    #     try:
-    #         self.scraper.driver.find_element(By.XPATH, '//*[@id= "gdpr-consent-notice"]')
-    #     except NoSuchElementException:
-    #         assert True
+    def test_accept_cookies(self):
+        try:
+            self.scraper.driver.find_element(By.XPATH, '//*[@id= "gdpr-consent-notice"]')
+        except NoSuchElementException:
+            assert True
 
-    # def test_search_ng8(self):
-    #     expected = 'https://www.zoopla.co.uk/for-sale/property/ng8/?q=NG8%20%20Nottingham%2C%20Wollaton%2C%20Aspley&search_source=home'
-    #     actual = self.scraper.driver.current_url
-    #     self.assertEqual(expected, actual)
-        
-    # def test_links_per_page(self):
-    #     expected = 25
-    #     actual = len(self.scraper.get_property_links())
-    #     self.assertEqual(expected, actual)
+    def test_search_ng8(self):
+        expected = 'https://www.zoopla.co.uk/for-sale/property/ng8/?q=NG8%20%20Nottingham%2C%20Wollaton%2C%20Aspley&search_source=home'
+        actual = self.scraper.driver.current_url
+        self.assertEqual(expected, actual)
 
-    # def test_change_page(self):
-    #     self.scraper.change_page()
-    #     time.sleep(2)
-    #     expected = 'https://www.zoopla.co.uk/for-sale/property/ng8/?q=NG8%20%20Nottingham%2C%20Wollaton%2C%20Aspley&search_source=home&pn=2'
-    #     actual = self.scraper.driver.current_url
-    #     self.assertEqual(expected, actual)
+    def test_change_page(self):
+        self.scraper.change_page()
+        time.sleep(2)
+        expected = 'https://www.zoopla.co.uk/for-sale/property/ng8/?q=NG8%20%20Nottingham%2C%20Wollaton%2C%20Aspley&search_source=home&pn=2'
+        actual = self.scraper.driver.current_url
+        self.assertEqual(expected, actual)
 
-    # def test_close_email_popup(self):
-    #     self.scraper.change_page()
-    #     self.scraper.close_email_popup()
-    #     try:
-    #         self.scraper.driver.find_element(By.XPATH, '//*[@class= "e1urp9zt0 css-1i01by8-StyledModal e13xjwxo11"]')
-    #     except NoSuchElementException:
-    #         assert True
-        
-    # def test_number_of_links_in_big_list(self):
-    #     counter = 0
-    #     while counter < 2:
-    #         if counter == 1:
-    #             self.scraper.close_email_popup()
-    #         property_list = self.scraper.get_property_links()
-    #         self.scraper.big_list.extend(property_list)
-    #         self.scraper.change_page()
-    #         counter += 1
-    #         time.sleep(2)
-    #     expected = 50
-    #     actual = len(self.scraper.big_list)
-    #     self.assertEqual(expected, actual)
+    def test_close_email_popup(self):
+        self.scraper.change_page()
+        self.scraper.close_email_popup()
+        try:
+            self.scraper.driver.find_element(By.XPATH, '//*[@class= "e1urp9zt0 css-1i01by8-StyledModal e13xjwxo11"]')
+        except NoSuchElementException:
+            assert True
 
     def test_data_file_contents(self):
-        self.scraper.create_raw_data_folder()
-        proplist = self.scraper.get_property_links()
-        time.sleep(2)
-        self.scraper.info_dict['Link'].append(proplist[0])
-        property_counter = 0
-        self.scraper.url = proplist[0]
-        print(proplist[0])
-        self.scraper.driver.get(self.scraper.url)
-        time.sleep(2)
-        self.scraper.get_property_info()
-        self.scraper.get_property_img()
-        self.scraper.get_unique_id()
-        self.scraper.get_uuid()
-        uid_directory = self.scraper.create_id_folders(property_counter)
-        data = self.scraper.create_data_files(uid_directory, property_counter)
-        data.seek(0)
-        content = data.read()
-        print(content)
-
+        uid_directory = self.scraper.get_info()
+        with open (f'{uid_directory}/data.json') as json_file:
+            data = json.load(json_file)
+        if 'Link' and 'Price' and 'Description' and 'Bathrooms' and 'Address' and 'IMG links' and 'UID' and 'UUID' in data:
+            assert True
+        else:
+            assert False
         
+    def test_images_downloaded(self):
+        uid_directory = self.scraper.get_info()
+        self.scraper.get_images(uid_directory)
+        img_dir = f'{uid_directory}/Images/'
+        expected = len(self.scraper.property_dict['Property'][-1]['IMG links'])
+        actual = (len([name for name in os.listdir(img_dir) if os.path.isfile(os.path.join(img_dir, name))]))
+        self.assertEqual(expected, actual)
 
 
 
